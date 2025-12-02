ba0a34cc05c127f5fc4bccb4057e7f14bbc803ad
Ashish Kumar <ashish@beltech.ai>
2025-12-03 03:52:11 +0530

    First commit

---
+++ 500-800/Fill Candies.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define inputarr(arr, n)                                                       \
+  for (int i = 0; i < n; i++)                                                  \
+    cin >> arr[i];
+#define endl "\n"
+#define pb push_back
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define vi(n)                                                                  \
+  vector<int> v(n);                                                            \
+  inputarr(v, n);
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k, m;
+    cin >> n >> k >> m;
+
+    int candies_in_one_bag = k * m;
+    int bags_req = (n + candies_in_one_bag - 1) / candies_in_one_bag;
+
+    cout << bags_req << endl;
+  }
+
+  return 0;
+}
